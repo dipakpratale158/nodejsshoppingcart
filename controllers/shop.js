43340d256fcd80c8ec0a33d468ced15dcea388cf
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-21 16:40:48 +0530

    add to cart quantity

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -10,12 +11,9 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-
-////////////////////reditect to hshop page when i am clicking to detail
-
 exports.getProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  Product.findById(productId, product => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
@@ -41,12 +39,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-
-exports.postCart=(req,res,next)=>{
-const prodid=req.body.productId
-console.log(prodid)
-res.redirect("/cart")
-}
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
